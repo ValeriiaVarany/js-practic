920c5319a2521911acef15fe4d242481e3b7acbe
ValeriiaVarany <62357877+ValeriiaVarany@users.noreply.github.com>
2021-07-06 13:51:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 
-//--------1. pobierz od użykownika liczbę i wyświetl mu czy jest parzysta czy nieparzysta-----------
+// zad 1
+function task1() { ///pobierz od użykownika liczbę i wyświetl mu czy jest parzysta czy nieparzysta
 
 let numberItem = prompt('please write a number','');
 
 (numberItem%2==0)? console.log("it is paired number"): console.log("it is not paired number");
+}
 
-//--------2. pobierz od użytkownika wyrazy  (ciągi liter oddzielonych spacją) i wyświetl mu złączone je camel casem
+task1()
 
+// zad 2  
 
+function task2() {  ///pobierz od użytkownika wyrazy  (ciągi liter oddzielonych spacją) i wyświetl mu złączone je camel casem
+      
 let letterItem = prompt('please write a world','');
-
-let camelCaseFunction = (world) =>console.log(world.toLowerCase().split(' ').join(''));
-
+let camelCaseFunction = (world) => console.log(world.toLowerCase().split(' ').join(''));
+      
 camelCaseFunction(letterItem);
+}
+task2()
 
-//-------3. pobierz od użytkownika tablice liczb oddzielonych spacją i wyświetl posortowaną tablicę oddzieloną spacją (może być sortowanie bąbelkowe)
+// zad 3
 
+function task3() {  ///pobierz od użytkownika tablice liczb oddzielonych spacją i wyświetl posortowaną tablicę oddzieloną spacją (może być sortowanie bąbelkowe)
+      
 let numberRange = prompt('Please, write a range of numbers','');
-
 let numberSortFunction = (number) => console.log(number.split(' ').sort((a,b) => (a-b)).join(' '));
 
 numberSortFunction(numberRange);
 
-//--------4.napisz funkcję, która przyjmuje wyrazy  (ciągi liter oddzielonych spacją) i wyświetla ile razy wystąpił dany wyraz zaczynając od najczęściej występującego słowa
-
+//zad 4
+     
 
 let items = "mleko kasza mleko banan mleko kasza";
-function amountCount(arrInputString){
+function task4(arrInputString){// napisz funkcję, która przyjmuje wyrazy  (ciągi liter oddzielonych spacją) i wyświetla ile razy wystąpił dany wyraz zaczynając od najczęściej występującego słowa
+
 let arrInput = arrInputString.split(' ');
 let uniqueItem = new Set(arrInput);
-
 let [...newArr] = uniqueItem;
-
-let amount = [];
-amount.length = newArr.length;
-amount.fill(0);
+let occurrences = [];
+occurrences.length = newArr.length;
+occurrences.fill(0);
 let finalArr = [];
 
-newArr.forEach((item, index)=> {
+newArr.forEach((item, index) => {
+      
 for (let i=0; i<=arrInput.length; i++){
-      (item == arrInput[i])? amount[index]+=1: amount[index]+=0;
+      (item == arrInput[i])? occurrences[index]+=1: occurrences[index]+=0;
   }
 });
 
 for (let i=0; i<=(newArr.length-1); i+=1){
-  finalArr.push(`${newArr[i]}(${amount[i]})`);
+  finalArr.push(`${newArr[i]}(${occurrences[i]})`);
 };
 
   console.log(finalArr.join(' '));
   
 };
-amountCount(items);
+task4(items);
